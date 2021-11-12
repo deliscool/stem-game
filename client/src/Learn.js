87db16337f799f676a5b188647156b73d360ac5b
kellyramos31 <72636939+kellyramos31@users.noreply.github.com>
2021-11-11 22:28:43 -0700

    commented out again

--- client/src/Learn.js
+++ client/src/Learn.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react"
+// import React, { useState } from "react"
+import React from "react"
 import gameData from "./gameQuestions"
 import ReactCardFlip from 'react-card-flip'
 import LearnFront from "./LearnFront"
@@ -8,17 +9,17 @@ import "./Learn.css"
 
 
 function Learn() {
-    const [isFlipped, setIsFlipped] = useState("false")
+    // const [isFlipped, setIsFlipped] = useState("false")
 
-   function handleClick(e) {
-        e.preventDefault();
-        setIsFlipped(prevState => ({ isFlipped: !prevState.isFlipped }));
-      }
+//    function handleClick(e) {
+//         e.preventDefault();
+        // setIsFlipped(prevState => ({ isFlipped: !prevState.isFlipped }));
+    //   }
 
     console.log(gameData)
     return (
         <div>
-            <ReactCardFlip isFlipped={isFlipped}>
+            <ReactCardFlip>
                 <LearnFront >
                 </LearnFront>
                 <LearnBack >
